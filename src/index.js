e94840938abae0967e93cdec1bef2ab59374112a
Jafr0 <zhek42@yandex.ru>
2022-02-24 19:17:55 +0300

    fix: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+	let arr = bracketsConfig.map(el => RegExp(`${del(el.join(''))}`, 'g'));
+
+	function del(el) {
+		return (el.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'))
+	};
+
+	let num;
+
+	do {
+		num = str.length;
+		str = remove(arr, str);
+	} while (str.length > 0 && str.length !== num);
+
+	function remove(arr, str) {
+		arr.forEach(i => { str = str.replace(i, ''); });
+
+		return str;
+	}
+
+	return str.length === 0;
+};
+
+
